a1c29d6713a593bc8141be534c9c29ff5b24d04e
fernando <fernandocto.scz@gmail.com>
2019-12-08 18:37:22 -0400

    Upload file with minio

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const debug = require('debug')('file-upload')
-const path = require('path')
 
 const http = require('http')
+const path = require('path')
+
 const morgan = require('morgan')
 const cors = require('cors')
 const compression = require('compression')
@@ -12,14 +13,19 @@ const nconf = require('nconf')
 const express = require('express')
 const fetch = require('node-fetch')
 const fileUpload = require('express-fileupload')
+const Minio = require('minio')
 
-const settings = nconf.file({ file: 'config/config.json' })
-const port = process.env.PORT || settings.get('settings').port
-const endpoint = settings.get('settings').endpoint || undefined
-const format = settings.get('settings').morgan || 'tiny'
-const directory = path.join(__dirname, settings.get('settings').directory || 'some-directory')
+const config = nconf.file({ file: 'config/config.json' })
+const port = process.env.PORT || config.get('settings').port
+const endpoint = config.get('settings').endpoint || undefined
+const format = config.get('settings').morgan || 'tiny'
+const directory = path.join(__dirname, config.get('settings').directory || 'some-directory')
 
-const { getUrlImage, verifyExistsImagen } = require('./utils')
+const { getUrlImage } = require('./utils')
+
+const bucket = config.get('minio').bucket || 'informatica'
+const connection = config.get('minio').connection || null
+const minioClient = new Minio.Client(connection)
 
 const app = express()
 const server = http.createServer(app)
@@ -29,39 +35,60 @@ app.use(cors())
 app.use(compression())
 app.use(fileUpload())
 
-app.get('/:image', function(req, res) {
+//TODO
+app.get('/:image', function (req, res) {
   const image = req.params.image
   getUrlImage(directory, image)
     .then(pathImage => res.sendFile(pathImage))
     .catch(err => res.status(500).json(err))
 })
 
-app.post('/upload', function(req, res) {
+app.post('/upload', function (req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).json({
       message: 'No files were uploaded'
     })
   }
+  if (!req.headers.user_id) {
+    return res.status(400).json({
+      message: 'User id is required.'
+    })
+  }
   const userId = req.headers.user_id
-  fetch(`${endpoint}/${userId}`)
+  const urlApi = `${endpoint}/${userId}`
+  fetch(urlApi)
     .then(response => response.json())
-    .then(body => {
-      const pathOld = `${directory}/${body.image}`
-      verifyExistsImagen(pathOld)
-      let sampleFile = req.files.sampleFile
-      const rename = `${new Date().getTime()}.${sampleFile.name.toLowerCase()}`
-      sampleFile.mv(`${directory}/${rename}`, (err) => {
+    .then(data => {
+      minioClient.removeObject(bucket, data.image, (err) => {
         if (err) {
-          return res.status(500).send(err)
+          res.end('Unable to remove object', err)
         }
-        res.json({
-          image: rename,
-          message: 'File uploaded!'
+        let sampleFile = req.files.sampleFile
+        const rename = `${new Date().getTime()}.${sampleFile.name.trim().toLowerCase()}`
+        minioClient.putObject(bucket, rename, sampleFile.data, (err, etag) => {
+          if (err) {
+            res.end("File upload error.")
+          }
+          fetch(urlApi, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ image: rename })
+          })
+            .then(() => {
+              res.json({
+                message: 'File uploaded successfully',
+                etag
+              })
+            })
+            .catch(err => res.status(500).json({
+              message: `Endpoint: ${endpoint} not found. (PATCH)`,
+              err
+            }))
         })
       })
     })
     .catch(err => res.status(500).json({
-      message: `Endpoint: ${endpoint} not found.`,
+      message: `Endpoint: ${endpoint} not found. (GET)`,
       err
     }))
 })
